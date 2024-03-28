ad19570f0ed5affa4464cbb1e7a58380a9f62a1d
antw0n_22 <151480241+acote4497@users.noreply.github.com>
2024-03-28 02:10:12 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <img src="public/assets/IMG_0336 2.png" alt="Logo" />
+                <a href="#">Anthony R. Cote</a>
+                <a className="selected" href="index.html">Home</a>
+                <a href="works.html">Works</a>
+                <a href="single-work.html">Single Work</a>
+                <a href="blog.html">Blog</a>
+                <a href="about.html">About</a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
