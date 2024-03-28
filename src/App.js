74080564d25fbd0fa586cf93dd263f75b5afed4a
antw0n_22 <151480241+acote4497@users.noreply.github.com>
2024-03-28 02:20:32 -0400

    Changed the files for execution problems.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header>
                 <img src="public/assets/IMG_0336 2.png" alt="Logo" />
                 <a href="#">Anthony R. Cote</a>
-                <a className="selected" href="index.html">Home</a>
+                <a href="index.html">Home</a>
                 <a href="works.html">Works</a>
                 <a href="single-work.html">Single Work</a>
                 <a href="blog.html">Blog</a>
